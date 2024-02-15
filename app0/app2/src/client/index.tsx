790ac802d3c64270a70f2a791d5be3a017cd0caa
Damir Karimov <dkarimov@alfabank.ru>
2024-02-15 17:49:19 +0300

    removed copies

--- app0/app2/src/client/index.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import { hydrateRoot } from "react-dom/client";
-import { Ui, rootDomNodeId } from "../ui";
-
-hydrateRoot(document.getElementById(rootDomNodeId), <Ui />);
