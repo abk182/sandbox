e9ae0e240fb923bbf3fe783fffca9ff1579ec1bb
Damir Karimov <dkarimov@alfabank.ru>
2024-02-15 10:59:05 +0300

    add style-loader

--- ui/src/app/target-income-form/index.tsx
+++ ui/src/app/target-income-form/index.tsx
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
 import { getTargetIncome } from "../../utils/get-target-salary";
 
+import './index.css';
+
 export const TargetIncomeForm = () => {
   const [currentIncome, setCurrentIncome] = useState(0);
   const [years, setYears] = useState(0);
 
   return (
-    <div>
+    <div className="form">
       <input
         placeholder="currentIncome"
         value={currentIncome}
--- ui/webpack.config.js
+++ ui/webpack.config.js
@@ -11,7 +11,7 @@ const config = {
       },
       {
         test: /\.css$/i,
-        use: ["css-loader"],
+        use: ["style-loader", "css-loader"],
         include: path.resolve(__dirname, "src"),
       },
     ],
