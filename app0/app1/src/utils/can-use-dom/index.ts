f6e930749973cc067c5c1b87be875b0ca4c37950
Damir Karimov <dkarimov@alfabank.ru>
2024-02-19 08:29:54 +0300

    copied

---
+++ app0/app1/src/utils/can-use-dom/index.ts
@@ -0,0 +1,14 @@
+export const canUseDom = () => {
+  try {
+    if (
+      typeof window !== "undefined" &&
+      window.document &&
+      window.document.createElement
+    ) {
+      return true;
+    }
+    return false;
+  } catch {
+    return false;
+  }
+};
