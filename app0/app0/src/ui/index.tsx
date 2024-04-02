e060c22fddd788bcd08c8180257bc4a88d975b7e
Damir Karimov <dkarimov@alfabank.ru>
2024-04-02 15:33:55 +0300

    up

--- app0/app0/src/ui/index.tsx
+++ app0/app0/src/ui/index.tsx
@@ -1,16 +1,12 @@
 import React from "react";
-import { TargetIncomeForm } from "./target-income-form";
 import useScriptLoader from "./script-loader";
 
 export const rootDomNodeId = "app0.app0";
+import styles from "./index.css";
 
 import "./index.global.css";
 
 export const Ui = () => {
   useScriptLoader("http://localhost:8081/assets/client.js");
-  return (
-    <div className={"ui"}>
-      <TargetIncomeForm />
-    </div>
-  );
+  return <div className={`ui ${styles.app0}`}>APP0 - shell</div>;
 };
