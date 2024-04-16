d597fddf648960e555dffacd46a8aafc8bbf4d77
Damir Karimov <dkarimov@alfabank.ru>
2024-04-16 16:54:51 +0300

    fix

--- app1/src/main.rs
+++ app1/src/main.rs
@@ -1,10 +1,11 @@
 fn main() {
-    failHelloWorld()
+    fail_hello_world()
 }
 
-fn failHelloWorld() {
+fn fail_hello_world() {
     let s1 = String::from("hello");
-    let s2 = s1;
+    let s2 = s1.clone();
 
     println!("{}, world!", s1);
+    println!("{}, world!", s2);
 }
