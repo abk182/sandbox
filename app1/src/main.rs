8355d5069d69d8042ecdf89e9308b99196913138
Damir Karimov <dkarimov@alfabank.ru>
2024-05-07 17:35:34 +0300

    up

--- app1/src/main.rs
+++ app1/src/main.rs
@@ -1,5 +1,28 @@
 fn main() {
-    try_reference()
+    try_slice();
+}
+
+fn try_slice() {
+    fn first_word(s: &String) -> usize {
+        let bytes = s.as_bytes();
+    
+        for (i, &item) in bytes.iter().enumerate() {
+            if item == b' ' {
+                return i;
+            }
+        }
+    
+        s.len()
+    }
+
+    let mut s = String::from("hello world");
+
+    let word = first_word(&s);
+
+    s.clear();
+
+    println!("{s}, {word}");
+
 }
 
 fn try_reference() {
