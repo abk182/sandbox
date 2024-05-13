96b5d3e5f53948b32e03a64132091b7dcf84abc0
Damir Karimov <dkarimov@alfabank.ru>
2024-05-13 10:53:39 +0300

    up

--- app4/src/lib.rs
+++ app4/src/lib.rs
@@ -4,13 +4,10 @@ use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 extern "C" {
-    fn alert(s: String);
+    fn alert(s: &str);
 }
 
 #[wasm_bindgen]
 pub fn greet(s: &str) {
-    let mut output = String::from("Hello, ");
-    output.push_str(s);
-    output.push_str(" !");
-    alert(output);
+    alert(&format!("Hello, {}!", s));
 }
