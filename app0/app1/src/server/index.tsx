a33b70c346383d3e9ded6cb0fdf55a9c5ca15463
Damir Karimov <dkarimov@alfabank.ru>
2024-05-13 15:22:22 +0300

    up

--- app0/app1/src/server/index.tsx
+++ app0/app1/src/server/index.tsx
@@ -5,27 +5,28 @@ import path from "path";
 import { Ui, rootDomNodeId } from "../ui";
 
 const startServer = () => {
-    const assetsFolderName = 'assets'
-    const port = 8081;
-    const app = express();
+  const assetsFolderName = "assets";
+  const assetsPath = `/${assetsFolderName}`;
+  const port = 8081;
+  const app = express();
 
-    app.use(`/${assetsFolderName}`, express.static(path.join(__dirname, `/${assetsFolderName}`)));
+  app.use(assetsPath, express.static(path.join(__dirname, assetsPath)));
 
-    app.get("/", (req, res) => {
-      res.end(
-        `<!DOCTYPE html>
+  app.get("/", (req, res) => {
+    res.end(
+      `<!DOCTYPE html>
           <html>
             <body>
               <div id=${rootDomNodeId}>${renderToString(<Ui />)}</div>
               <script type="text/javascript" src="${assetsFolderName}/client.js"></script>
             </body>
           </html>`
-      );
-    });
+    );
+  });
 
-    app.listen(port, () => {
-      console.log(`On port ${port}`);
-    });
+  app.listen(port, () => {
+    console.log(`On port ${port}`);
+  });
 };
 
 startServer();
