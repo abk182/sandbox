ff02f4d41a13b1ad1ff466b556c0c4fb02f6fa67
Damir Karimov <dkarimov@alfabank.ru>
2024-05-16 17:41:41 +0300

    up

--- app1/src/main.rs
+++ app1/src/main.rs
@@ -1,20 +1,40 @@
 fn main() {
     let rect = Rectangle {
         width: 11,
-        height: 2
+        height: 2,
+        area: 23
     };
 
     dbg!(&rect);
     println!("rect {:#?}", rect);
-    println!("area: {0}", area(&rect));
-}
-
-fn area(dimensions: &Rectangle) -> u32 {
-    dimensions.width * dimensions.height
+    println!("area: {0}", rect.area());
+    if rect.check_area() {
+        println!("area ok")
+    } else {
+        println!("area not ok")
+    }
+    rect.take_ownership_of_self();
+    // rect.take_ownership_of_sefl(); // wont work, borrowing
 }
 
 #[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
+    area: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn check_area(&self) -> bool {
+        self.area() == self.area
+    }
+
+    fn take_ownership_of_self(self) -> u32 {
+        self.area()
+    }
+
 }
