7fa4a5696f89031d1a20fcc3d271b936df1d9205
Damir Karimov <dkarimov@alfabank.ru>
2024-05-16 10:13:59 +0300

    up

--- app1/src/main.rs
+++ app1/src/main.rs
@@ -1,7 +1,8 @@
 fn main() {
-    println!("area: {0}", area(2, 3));
+    let rect = (2, 3);
+    println!("area: {0}", area(rect));
 }
 
-fn area(width: u32, height: u32) -> u32 {
-    width * height
+fn area(dimensions: (u32, u32)) -> u32 {
+    dimensions.0 * dimensions.1
 }
