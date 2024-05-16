970a2b090e6b6715457f935647c5a0e8c6d010d4
Damir Karimov <dkarimov@alfabank.ru>
2024-05-16 10:10:17 +0300

    up

--- app1/src/main.rs
+++ app1/src/main.rs
@@ -1,45 +1,7 @@
 fn main() {
-    try_struct()
+    println!("area: {0}", area(2, 3));
 }
 
-enum Status {
-    Alive = 1,
-    Dead = 0
-}
-
-struct Birth(i32, i32, i32);
-
-struct User {
-    status: Status,
-    username: String,
-    email: String,
-    sign_in_count: u64,
-    birth: Birth
-}
-
-fn try_struct() {
-    struct EmptyStruct;
-    let emptyStruct = EmptyStruct;
-
-    let mut user0 = User {
-        status: Status::Dead,
-        username: String::from("Username"),
-        email: String::from("email@mail.com"),
-        sign_in_count: 0,
-        birth: Birth(16, 12, 92)
-    }; 
-
-    let mut user1 = User {
-        status: Status::Alive,
-        sign_in_count: user0.sign_in_count + 1,
-        ..user0
-    };
-
-    user0.username = String::from("*");
-    user1.email = String::from("awesome@mail.com");
-
-    println!("{0}", user0.username); 
-    // println!("{0}", user0.birth.0); wont work, borrowing
-    // println!("{0}", user0.email); wont work, borrowing
-    println!("{0}, {1}.{2}.{3}", user1.username, user1.birth.0, user1.birth.1, user1.birth.2);
+fn area(width: u32, height: u32) -> u32 {
+    width * height
 }
