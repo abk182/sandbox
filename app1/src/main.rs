62bd9719048d3fbde39b41d1bd6367f72ada1df4
Damir Karimov <dkarimov@alfabank.ru>
2024-05-17 11:47:32 +0300

    up

--- app1/src/main.rs
+++ app1/src/main.rs
@@ -1,11 +1,17 @@
 fn main() {
+    #[derive(Debug)]
+    enum UsState {
+        Alabama,
+        Alaska,
+    }
+
     enum Coin {
         Penny,
         Nickel,
         Dime,
-        Quarter,
+        Quarter(UsState),
     }
-    
+
     fn value_in_cents(coin: Coin) -> u8 {
         match coin {
             Coin::Penny => {
@@ -14,9 +20,12 @@ fn main() {
             }
             Coin::Nickel => 5,
             Coin::Dime => 10,
-            Coin::Quarter => 25,
+            Coin::Quarter(s) => {
+                println!("{:?}", s);
+                25
+            }
         }
     }
 
-    value_in_cents(Coin::Penny);
+    value_in_cents(Coin::Quarter(UsState::Alabama));
 }
