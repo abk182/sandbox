beebac88db68b9e20b937725f481a14486c6bd40
Damir Karimov <dkarimov@alfabank.ru>
2024-05-23 16:30:10 +0300

    up

---
+++ app1/restaurant/src/front_of_house.rs
@@ -0,0 +1,12 @@
+
+pub fn try_same_name() {}
+
+pub mod hosting;
+
+pub mod serving {
+    pub fn take_order() {}
+
+    fn serve_order() {}
+
+    fn take_payment() {}
+}
--- app1/restaurant/src/lib.rs
+++ app1/restaurant/src/lib.rs
@@ -1,55 +1,7 @@
-fn deliver_order() {}
-mod front_of_house {
-    pub fn try_same_name() {}
+mod front_of_house;
+mod back_of_house;
 
-    pub mod hosting {
-        pub fn add_to_waitlist() {}
-
-        fn seat_at_table() {}
-    }
-
-    pub mod serving {
-        pub fn take_order() {}
-
-        fn serve_order() {}
-
-        fn take_payment() {}
-    }
-}
-
-mod back_of_house {
-    pub fn try_same_name() {}
-
-    fn fix_incorrect_order() {
-        cook_order();
-        super::deliver_order();
-    }
-
-    fn cook_order() {}
-
-    pub struct Breakfast {
-        pub toast: String,
-        seasonal_fruit: String,
-    }
-
-    impl Breakfast {
-        pub fn summer(toast: &str) -> Breakfast {
-            Breakfast {
-                toast: String::from(toast),
-                seasonal_fruit: String::from("peaches"),
-            }
-        }
-    }
-
-    pub enum Appetizer {
-        Soup,
-        Salad,
-    }
-}
-pub use crate::front_of_house::{hosting, serving::{self, *}};
-// pub use crate::front_of_house::{hosting, serving::{self, take_order}};
-use back_of_house::try_same_name as b_try_same_name;
-use front_of_house::try_same_name as t_try_same_name;
+pub use crate::front_of_house::hosting;
 
 pub fn eat_at_restaurant() {
     // Absolute path
@@ -73,3 +25,5 @@ pub fn eat_at_restaurant() {
     let order1 = back_of_house::Appetizer::Soup;
     let order2 = back_of_house::Appetizer::Salad;
 }
+
+fn deliver_order() {}
