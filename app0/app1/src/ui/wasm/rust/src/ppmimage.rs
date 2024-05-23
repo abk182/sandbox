3f8167cfc0b235ba7bcabc50becab92156d3c280
Damir Karimov <dkarimov@alfabank.ru>
2024-05-23 11:18:00 +0300

    add fn

--- app0/app1/src/ui/wasm/rust/src/ppmimage.rs
+++ app0/app1/src/ui/wasm/rust/src/ppmimage.rs
@@ -6,20 +6,23 @@ pub fn draw_ppm_image(width: u16, height: u16) -> String {
 
     output.push_str(&format!("P3\n{} {}\n255\n", width, height));
 
-    for j in (0..height).rev() {
+    for j in 0..height {
         for i in 0..width {
-            let r = i as f64 / (width - 1) as f64;
-            let g = j as f64 / (height - 1) as f64;
-            let b = 0.25;
-
-            let ir = (255.99 * r) as u8;
-            let ig = (255.99 * g) as u8;
-            let ib = (255.99 * b) as u8;
-            
-
-            output.push_str(&format!("{} {} {}\n", ir, ig, ib));
+            draw_pixel(i, j, width, height, &mut output);
         }
     }
-    
+
     output
+}
+
+fn draw_pixel(x: u16, y: u16, width: u16, height: u16, s: &mut String) {
+    let r = x as f64 / (width - 1) as f64;
+    let g = y as f64 / (height - 1) as f64;
+    let b = 0.25;
+
+    let ir = (255.99 * r) as u8;
+    let ig = (255.99 * g) as u8;
+    let ib = (255.99 * b) as u8;
+
+    s.push_str(&format!("{} {} {}\n", ir, ig, ib));
 }
