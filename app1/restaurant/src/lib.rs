971ba9672c448a528cd2c957f1a4a6a02c86049d
Damir Karimov <dkarimov@alfabank.ru>
2024-05-23 16:11:36 +0300

    up

--- app1/restaurant/src/lib.rs
+++ app1/restaurant/src/lib.rs
@@ -1,12 +1,15 @@
+fn deliver_order() {}
 mod front_of_house {
+    pub fn try_same_name() {}
+
     pub mod hosting {
         pub fn add_to_waitlist() {}
 
         fn seat_at_table() {}
     }
 
-    mod serving {
-        fn take_order() {}
+    pub mod serving {
+        pub fn take_order() {}
 
         fn serve_order() {}
 
@@ -14,10 +17,59 @@ mod front_of_house {
     }
 }
 
+mod back_of_house {
+    pub fn try_same_name() {}
+
+    fn fix_incorrect_order() {
+        cook_order();
+        super::deliver_order();
+    }
+
+    fn cook_order() {}
+
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+
+    pub enum Appetizer {
+        Soup,
+        Salad,
+    }
+}
+pub use crate::front_of_house::{hosting, serving::{self, *}};
+// pub use crate::front_of_house::{hosting, serving::{self, take_order}};
+use back_of_house::try_same_name as b_try_same_name;
+use front_of_house::try_same_name as t_try_same_name;
+
 pub fn eat_at_restaurant() {
     // Absolute path
     crate::front_of_house::hosting::add_to_waitlist();
 
     // Relative path
     front_of_house::hosting::add_to_waitlist();
+
+    // use
+    hosting::add_to_waitlist();
+
+    // break;
+    // let mut meal = back_of_house::Breakfast {
+    //     toast: String::from("Rye"),
+    // };
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
+    // meal.seasonal_fruit = String::from("blueberries"); break;
+
+    let order1 = back_of_house::Appetizer::Soup;
+    let order2 = back_of_house::Appetizer::Salad;
 }
