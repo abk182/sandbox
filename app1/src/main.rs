8074ea04c3b7da8066e473577d39c7e78621c9b7
Damir Karimov <dkarimov@alfabank.ru>
2024-05-24 14:57:26 +0300

    up

--- app1/src/main.rs
+++ app1/src/main.rs
@@ -46,4 +46,47 @@ fn main() {
     for b in "Рщш".bytes() {
         println!("{b}");
     }
+
+    use std::collections::HashMap;
+
+    let mut scores = HashMap::new();
+
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Blue"), 25);
+    scores.entry(String::from("Yellow")).or_insert(99);
+    scores.entry(String::from("Blue")).or_insert(99);
+
+    println!("{:?}", scores);
+    
+    for (key, value) in &scores {
+        println!("{key}: {value}");
+    }
+
+    println!("score: {}", scores[&String::from("Yellow")]);
+
+    let team_name = String::from("Blue");
+    let score = scores.get(&team_name).copied().unwrap_or(0);
+    println!("score: {score}");
+
+    let field_name = String::from("Favorite color");
+    let field_value = String::from("Blue");
+
+    let mut map = HashMap::new();
+    map.insert(field_name, field_value);
+    // break
+    // println!("{field_name}, {field_value}")
+
+
+    let text = "hello world wonderful world";
+
+    let mut map = HashMap::new();
+
+    for word in text.split_whitespace() {
+        let count = map.entry(word).or_insert(0);
+        *count += 1;
+    }
+
+    println!("{:?}", map);    
 }
