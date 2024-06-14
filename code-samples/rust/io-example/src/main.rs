f7ffd17500c4c9de89364c575ae57a706509dd02
Damir Karimov <dkarimov@alfabank.ru>
2024-06-14 14:41:29 +0300

    up

--- code-samples/rust/io-example/src/main.rs
+++ code-samples/rust/io-example/src/main.rs
@@ -4,11 +4,11 @@ use app1::{Config, run};
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
     if let Err(e) = run(config) {
-        println!("Application error: {e}");
+        eprintln!("Problem parsing arguments: {e}");
         process::exit(1);
     }
 }
