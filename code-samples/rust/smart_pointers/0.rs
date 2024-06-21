1cf75f4040934dca53a39bec822c9a26e3e161ef
Damir Karimov <dkarimov@alfabank.ru>
2024-06-21 09:36:32 +0300

    up

--- code-samples/rust/smart_pointers/0.rs
+++ code-samples/rust/smart_pointers/0.rs
@@ -4,7 +4,38 @@ enum List {
 }
 
 use crate::List::{Cons, Nil};
+use std::ops::Deref;
 
 fn main() {
-    let list = Cons(1,  Box::new(Cons(2,  Box::new(Cons(3,  Box::new(Nil))))));
+    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+
+    let x = 5;
+    let mut y = &x;
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+
+    struct MyBox<T>(T);
+    impl<T> MyBox<T> {
+        fn new(x: T) -> MyBox<T> {
+            MyBox(x)
+        }
+    }
+    impl<T> Deref for MyBox<T> {
+        type Target = T;
+
+        fn deref(&self) -> &Self::Target {
+            &self.0
+        }
+    }
+
+    let x = 5;
+    let y = MyBox::new(x);
+    assert_eq!(5, x);
+    assert_eq!(5, *y); // assert_eq!(5, *(y.deref()));
+    
+    fn hello(name: &str) {
+        println!("Hello, {name}!");
+    }
+    let m = MyBox::new(String::from("Rust"));
+    hello(&m);
 }
