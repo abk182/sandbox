db20ddaa9db55a36c1818b95d8fc1d9142d5e29e
Damir Karimov <dkarimov@alfabank.ru>
2024-06-24 15:50:01 +0300

    up

--- code-samples/rust/smart_pointers/3.rs
+++ code-samples/rust/smart_pointers/3.rs
@@ -7,13 +7,15 @@ use crate::List::{Cons, Nil};
 use std::rc::Rc;
 
 fn main() {
-    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
-    println!("count after creating a = {}", Rc::strong_count(&a));
-    let b = Cons(3, Rc::clone(&a));
-    println!("count after creating b = {}", Rc::strong_count(&a));
+    let a = Cons(5, Rc::new(Cons(10, Rc::new(Nil))));
+    
+    let a_rc = Rc::new(a);
+    println!("count after creating a_rs = {}", Rc::strong_count(&a_rc));
+    let b = Cons(3, Rc::clone(&a_rc));
+    println!("count after creating b = {}", Rc::strong_count(&a_rc));
     {
-        let c = Cons(4, Rc::clone(&a));
-        println!("count after creating c = {}", Rc::strong_count(&a));
+        let c = Cons(4, Rc::clone(&a_rc));
+        println!("count after creating c = {}", Rc::strong_count(&a_rc));
     }
-    println!("count after c goes out of scope = {}", Rc::strong_count(&a));
+    println!("count after c goes out of scope = {}", Rc::strong_count(&a_rc));
 }
