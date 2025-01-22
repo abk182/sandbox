9a2e088b60f639971ca74490fe6571eca66d66af
Damir Karimov <dkarimov@alfabank.ru>
2025-01-22 17:59:39 +0300

    implemented life

--- wasm-example/www/index.js
+++ wasm-example/www/index.js
@@ -1,3 +1,13 @@
-import * as wasm from "wasm-app4";
+import { Universe } from "wasm-game-of-life";
 
-wasm.greet('awesome greetings');
+const pre = document.getElementById("game-of-life-canvas");
+const universe = Universe.new();
+
+const renderLoop = () => {
+  pre.textContent = universe.render();
+  universe.tick();
+
+  requestAnimationFrame(renderLoop);
+};
+
+requestAnimationFrame(renderLoop);
